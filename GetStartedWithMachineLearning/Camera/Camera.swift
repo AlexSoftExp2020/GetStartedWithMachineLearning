253141291a94753ba22d1a3123789b8f46bcc13b
Alex <oleksii.pidhornyi@gmail.com>
2025-02-16 22:45:08 +0100

    add uiscreen extension

--- GetStartedWithMachineLearning/Camera/Camera.swift
+++ GetStartedWithMachineLearning/Camera/Camera.swift
@@ -367,3 +367,21 @@ extension Camera: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
 }
+
+fileprivate extension UIScreen {
+
+    var orientation: UIDeviceOrientation {
+        let point = coordinateSpace.convert(CGPoint.zero, to: fixedCoordinateSpace)
+        if point == CGPoint.zero {
+            return .portrait
+        } else if point.x != 0 && point.y != 0 {
+            return .portraitUpsideDown
+        } else if point.x == 0 && point.y != 0 {
+            return .landscapeRight //.landscapeLeft
+        } else if point.x != 0 && point.y == 0 {
+            return .landscapeLeft //.landscapeRight
+        } else {
+            return .unknown
+        }
+    }
+}
