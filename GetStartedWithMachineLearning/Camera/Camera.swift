973ada8e2842d33d74760e090f064d19629425cb
Alex <oleksii.pidhornyi@gmail.com>
2025-02-16 22:36:12 +0100

    add start function in camera

--- GetStartedWithMachineLearning/Camera/Camera.swift
+++ GetStartedWithMachineLearning/Camera/Camera.swift
@@ -231,4 +231,32 @@ class Camera: NSObject {
             }
         }
     }
+    
+    func start() async {
+        let authorized = await checkAuthorization()
+        guard authorized else {
+            logger.error("Camera access was not authorized.")
+            return
+        }
+        
+        if isCaptureSessionConfigured {
+            if !captureSession.isRunning {
+                sessionQueue.async { [self] in
+                    self.captureSession.startRunning()
+                }
+            }
+            return
+        }
+        
+        sessionQueue.async { [self] in
+            if captureDevice == nil {
+                captureDevice = availableCaptureDevices.first ?? AVCaptureDevice.default(for: .video)
+            }
+
+            self.configureCaptureSession { success in
+                guard success else { return }
+                self.captureSession.startRunning()
+            }
+        }
+    }
 }
