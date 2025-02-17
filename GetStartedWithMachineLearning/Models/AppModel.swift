c3f589f27635df5bb6ca620e6f4d95eec80c8adb
Alex <oleksii.pidhornyi@gmail.com>
2025-02-17 10:59:34 +0100

    add ciimage extension

--- GetStartedWithMachineLearning/Models/AppModel.swift
+++ GetStartedWithMachineLearning/Models/AppModel.swift
@@ -174,5 +174,13 @@ extension AppModel: MLDelegate {
     }
 }
 
-
+fileprivate extension CIImage {
+    var image: Image? {
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(self, from: self.extent) else {
+            return nil
+        }
+        return Image(decorative: cgImage, scale: 1, orientation: .up)
+    }
+}
 
