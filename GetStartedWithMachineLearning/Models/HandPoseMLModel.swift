72a1ab26568c32ba33c4731e36537f2dcd48b229
Alex <oleksii.pidhornyi@gmail.com>
2025-02-18 22:39:28 +0100

    add hand pose ml model

---
+++ GetStartedWithMachineLearning/Models/HandPoseMLModel.swift
@@ -0,0 +1,31 @@
+//
+//  HandPoseMLModel.swift
+//  GetStartedWithMachineLearning
+//
+//  Created by Oleksii on 18.02.2025.
+//
+
+import Foundation
+import CoreML
+
+final class HandPoseMLModel: NSObject, Identifiable {
+    let name: String
+    let mlModel: MLModel
+    let url: URL
+    
+    private var classLabels: [Any] {
+        mlModel.modelDescription.classLabels ?? []
+    }
+
+    init(name: String, mlModel: MLModel, url: URL) {
+        self.name = name
+        self.mlModel = mlModel
+        self.url = url
+    }
+
+    func predict(poses: HandPoseInput) throws -> HandPoseOutput? {
+        let features = try mlModel.prediction(from: poses)
+        let output = HandPoseOutput(features: features)
+        return output
+    }
+}
