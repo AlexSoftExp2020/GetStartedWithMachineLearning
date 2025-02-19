5c2ee0fbb4c9b46e7d81aef6f39b1e01ed5a88c0
Alex <oleksii.pidhornyi@gmail.com>
2025-02-19 10:16:34 +0100

    add color extension

---
+++ GetStartedWithMachineLearning/Support/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  GetStartedWithMachineLearning
+//
+//  Created by Oleksii on 19.02.2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static var strokeColor: Color { white.opacity(0.5) }
+    static var translucentBlack: Color { black.opacity(0.5) }
+    static var accent: Color { Color.indigo }
+    static var labelAccent: Color { Color(red: 0.224, green: 0.0, blue: 0.263) }
+}
+
