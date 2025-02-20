68c74c4e5e3e5dc70e88e728394171987b076ad0
Alex <oleksii.pidhornyi@gmail.com>
2025-02-20 13:34:01 +0100

    add private functions

--- GetStartedWithMachineLearning/Views/Other/HandPoseNodeOverlay.swift
+++ GetStartedWithMachineLearning/Views/Other/HandPoseNodeOverlay.swift
@@ -41,4 +41,31 @@ struct HandPoseNodeOverlay: View {
             }
         }
     }
+    
+    private func updatePoint(_ point: CGPoint, viewSize: CGSize) -> CGPoint {
+        let currentFrameAspectRatio = viewSize.width / viewSize.height
+        if currentFrameAspectRatio > imageAspectRatio {
+            return CGPoint(x: (point.x * viewSize.width),
+                           y: ((1 - point.y) * scaledHeight(viewSize)) - yOffset(viewSize))
+        }
+
+        return CGPoint(x: (point.x * scaledWidth(viewSize)) - xOffset(viewSize),
+                       y: (1 - point.y) * viewSize.height)
+    }
+
+    private func scaledWidth(_ viewSize: CGSize) -> CGFloat {
+        return imageAspectRatio * viewSize.height
+    }
+
+    private func scaledHeight(_ viewSize: CGSize) -> CGFloat {
+        return (1 / imageAspectRatio) * viewSize.width
+    }
+
+    private func xOffset(_ viewSize: CGSize) -> CGFloat {
+        return (scaledWidth(viewSize) - viewSize.width) / 2
+    }
+
+    private func yOffset(_ viewSize: CGSize) -> CGFloat {
+        return (scaledHeight(viewSize) - viewSize.height) / 2
+    }
 }
